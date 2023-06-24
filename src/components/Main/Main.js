0de85b41fc7b82501041e97edaffc3a7a1006fac
Anton Alokhin <anton.alyokhin1997@gmail.com>
2023-06-24 19:42:48 +0300

    24.06.2023.19:42

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import { MainBg } from './Main.styled';
-import { HelloTitle, Text, ListItem, TextBox } from './Main.styled';
+import { HelloTitle, Text, TextBox } from './Main.styled';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 const Main = () => {
@@ -9,15 +9,8 @@ const Main = () => {
         <HelloTitle>Hello!</HelloTitle>
         <Text>
           I am your faithful assistant <InsertEmoticonIcon />, your phone book!
-          Register and you will have many advantages:
+          Register and you will have many advantages!
         </Text>
-        <ul>
-          <ListItem>All your contacts in one place.</ListItem>
-          <ListItem>Easy access to the site from any device</ListItem>
-          <ListItem>Convenient navigation</ListItem>
-          <ListItem>Search filter</ListItem>
-          <ListItem>Nice design</ListItem>
-        </ul>
       </TextBox>
     </MainBg>
   );
